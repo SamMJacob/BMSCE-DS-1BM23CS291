6fdd1c5537359799dc1fe88e63d44c7674df3b1a
SamMJacob <146120163+SamMJacob@users.noreply.github.com>
2024-09-30 12:55:02 +0530

    Update Stack_Operation.c

--- Stack_Operation.c
+++ Stack_Operation.c
@@ -1,3 +1,4 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
@@ -9,71 +10,79 @@ int main() {
     int value;
     int opt=0;
     
-    while(opt!=6)
+    while(opt!=7)
     {
-      printf("enter 1 to PUSH, 2 to POP, 3 to SEEK, 4 to check IfEmpty and 5 to check IfFull and 6 to exit");
-      scanf("%d",&opt);
-      
-      if (opt==1)
-      {
-          if (top==max-1)
-          {
-              printf("stack overflow\n");
-          }
-          else 
-          {
-              printf("enter value to be pushed");
-              scanf("%d",&value);
-              top=top+1;
-              stack[top]=value;
-          }
-      }
-      else if (opt==2)
-      {   
-          if (top==-1)
-          {
-              printf("stack underflow\n");
-          }
-          else 
-          {
-              value= stack[top];
-              top=top-1;
-              printf("%d was popped\n",value);
-          }
-          
-      }
-      
-      else if(opt==3)
-      {   
-          if(top==-1)
-          {
-              printf("stack is empty\n");
-          }
-          else
-          {
-              value = stack[top];
-              printf("top- most element is %d \n",value);
-          }
-      }
-      else if (opt==4)
-      {
-          if(top==-1)
-          {
-              printf("stack is empty\n");
-          }
-          else
-          {
-              printf("stack is not empty\n");
-          }
-      }
-      else if (opt==5)
-      {
-           if (top==max-1)
-          {
-              printf("stack is full\n");
-          }
-          else 
-          printf("stack is not full\n");
-      }
+    printf("enter 1 to PUSH, 2 to POP, 3 to SEEK, 4 to check IfEmpty and 5 to check IfFull and 7 to exit");
+    scanf("%d",&opt);
+    
+    if (opt==1)
+    {
+        if (top==max-1)
+        {
+            printf("stack overflow\n");
+        }
+        else 
+        {
+            printf("enter value to be pushed");
+            scanf("%d",&value);
+            top=top+1;
+            stack[top]=value;
+        }
+    }
+    else if (opt==2)
+    {   
+        if (top==-1)
+        {
+            printf("stack underflow\n");
+        }
+        else 
+        {
+            value= stack[top];
+            top=top-1;
+            printf("%d was popped\n",value);
+        }
+        
+    }
+    
+    else if(opt==3)
+    {   
+        if(top==-1)
+        {
+            printf("stack is empty\n");
+        }
+        else
+        {
+            value = stack[top];
+            printf("top- most element is %d \n",value);
+        }
+    }
+    else if (opt==4)
+    {
+        if(top==-1)
+        {
+            printf("stack is empty\n");
+        }
+        else
+        {
+            printf("stack is not empty\n");
+        }
+    }
+    else if (opt==5)
+    {
+         if (top==max-1)
+        {
+            printf("stack is full\n");
+        }
+        else 
+        printf("stack is not full\n");
+    }
+    else if(opt==6)
+    {
+        for(int i=0;i<=top;i++)
+        {
+            printf("%d,",stack[i]);
+        }
+        printf("\n");
+    }
     }
 }
